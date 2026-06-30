64e7551a2dddd73fd597758aa79143353a2faba4
Merouane Atig <merwan@patem.net>
2026-06-30 15:22:09 +0200

    Retrict access by IP

--- src/main/java/com/github/uhlive/keycloak/ipauthenticator/IPAuthenticator.java
+++ src/main/java/com/github/uhlive/keycloak/ipauthenticator/IPAuthenticator.java
@@ -1,16 +1,20 @@
 package com.github.uhlive.keycloak.ipauthenticator;
 
-import java.util.Collections;
-import java.util.Map;
+import java.util.List;
 
 import org.jboss.logging.Logger;
 import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.AuthenticationFlowError;
 import org.keycloak.authentication.Authenticator;
-import org.keycloak.models.*;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
 
 public class IPAuthenticator implements Authenticator {
 
     private static final Logger logger = Logger.getLogger(IPAuthenticator.class);
+
+    private static final String IP_ADDRESSES_ATTRIBUTE = "allowed_ips";
 
     @Override
     public void authenticate(AuthenticationFlowContext context) {
@@ -18,11 +22,21 @@ public class IPAuthenticator implements Authenticator {
         RealmModel realm = context.getRealm();
         UserModel user = context.getUser();
 
+        // TODO: get the attribute value to look for from the authenticator config
+        List<String> ipAddresses = user.getAttributeStream(IP_ADDRESSES_ATTRIBUTE).toList();
+        for (String ip : ipAddresses) {
+            logger.infof("Attribute value %s", ip);
+        }
+        // TODO: if no address in user attributes => success
         String remoteIPAddress = context.getConnection().getRemoteAddr();
+        logger.infof("********** Realm %s, user %s logged from %s", realm.getName(), user.getUsername(), remoteIPAddress);
+        if (ipAddresses.contains(remoteIPAddress)) {
+            logger.infof("Remote IP %s match", remoteIPAddress);
+            context.success();
+        }
 
-        logger.infof("Nothing for now. Realm %s, user %s logged from %s", realm.getName(), user.getUsername(), remoteIPAddress);
-
-        context.success();
+        logger.info("################# ACCESS DENIED ################");
+        context.failure(AuthenticationFlowError.ACCESS_DENIED);
     }
 
     @Override
